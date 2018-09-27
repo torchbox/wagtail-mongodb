2cb8b873f8a642574a5175eb2cd0a9b3606b85b4
Mitchel Cabuloy <mixxorz@gmail.com>
2018-09-27 18:03:40 +0800

    Remove debugging code

--- mongodb_blog/blog/models.py
+++ mongodb_blog/blog/models.py
@@ -77,8 +77,6 @@ class BlogIndex(RoutablePageMixin, BasePage):
             .order_by('-date')
         )
 
-        posts = list(posts) * 50
-
         # Pagination
         page = request.GET.get('page', 1)
         paginator = Paginator(posts, settings.DEFAULT_PER_PAGE)
